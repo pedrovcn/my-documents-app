17759843950d4e0dc7e204269da1438d691576a8
Pedro Nascimento <pedrovcn@gmail.com>
2018-04-13 19:14:15 -0300

    Initial Commit

---
+++ myDocuments/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myDocuments
+//
+//  Created by Pedro Nascimento on 13/04/2018.
+//  Copyright © 2018 Pedro Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
